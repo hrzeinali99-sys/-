862b9385e74fee8bcb496fb4ebdf96775124e2be
hrzeinali99-sys <hrzeinali99@gmail.com>
2026-09-03 09:24:57 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/-/',          // ← add this
+    base: '/hr-system/',          // ← add this
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
